0b0fcbd8449bafb1f4af266b2e032d315bcaa159
Walisson Rodrigo <rodrigo@sagamedicao.com.br>
2017-11-22 02:06:57 -0200

    Criação de Layout Padrão

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,17 +11,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        blog\Models\User::states('admin')
-            ->create([
+        blog\Models\User::create([
                 'name'=>'admin',
                 'email'=>'admin@admin',
-                'password'=>bcrypt('admin')
+                'password'=>bcrypt('admin'),
+                'role'=>'admin'
             ]);
-        blog\Models\User::states('admin')
-            ->create([
+        blog\Models\User::create([
                 'name'=>'Walisson Rodrigo',
                 'email'=>'walissonrodrigo@outlook.com',
-                'password'=>bcrypt('97498037')
+                'password'=>bcrypt('97498037'),
+                'role'=>'admin'
             ]);
 
         blog\Models\User::create([

0fbe900d2c42f25271a1444434cb7e4178be7309
Walisson Rodrigo <rodrigo@sagamedicao.com.br>
2017-11-22 11:06:07 -0200

    Verificação de Pagina do Google

--- resources/views/blog/layout/head.blade.php
+++ resources/views/blog/layout/head.blade.php
@@ -1,6 +1,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <meta name="google-site-verification" content="z1KbkHEdJj-BJDxIOodhQTgc_FmZFHX4ZAvollyLR7I" />
   <title>{{ config('app.name', 'Blog') }}</title>
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">

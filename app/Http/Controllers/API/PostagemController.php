f18065709ba645c59bf7988b381d91e3b6f60341
Walisson Rodrigo <walissonrodrigo@outlook.com>
2018-06-14 01:22:40 -0300

    inclusão de api para postagems

---
+++ app/Http/Controllers/API/PostagemController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace blog\Http\Controllers\API;
+
+use Auth;
+use blog\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use blog\Models\Posts;
+use blog\Models\Comentarios;
+
+class PostagemController extends Controller
+{
+
+    private $posts;
+    private $comment;
+
+    public function __construct(Posts $posts, Comentarios $comment){
+        $this->posts = $posts;
+        $this->comment = $comment;
+        $this->middleware('guest');
+    }
+
+    public function index()
+    {
+        $posts = $this->posts->orderBy('updated_at', 'desc')->paginate(5);
+        return response()->json($posts);
+    }
+
+    public function postagem($post_id)
+    {
+        $posts = $this->posts->find($post_id);
+        return response()->json($posts);
+    }
+
+    /*
+
+    public function editar($id)
+    {
+        $post = $this->posts->find($id);
+        return view('blog.posts.create-edit', compact('post'));
+    }
+
+    public function postar(Request $request)
+    {
+        view('blog.posts.create-edit');
+    }
+
+    public function armazenar()
+    {
+
+    }
+
+    public function atualizar()
+    {
+
+    }
+    
+    public function comentar(Request $request, $idPost)
+    {
+        $this->comment->create($request->all());
+        return redirect()->route('/detail', $request->post_id);
+    }
+
+    public function editarComentario($idPost, $idComentario, Request $request)
+    {
+        $form = $request->except('_token');
+        $comments = $this->comment->find($idComentario);
+        $update = $comments->update($form);
+        if($update)
+            return redirect()->action('Blog\BlogController@detalhes', $idPost)->with(['success'=>'Seu comentário foi Editado com Sucesso!']);
+        else
+            return redirect()->back()->with(['erros'=>'Não foi possivel editar seu comentário. Tente mais tarde!']);
+    }
+
+    */
+}
